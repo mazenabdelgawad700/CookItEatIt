cb2406cbc1385bde66d0eaf5f70f0c6252b3739a
mazen abdelgawad <mazenabdelgawad700@gmail.com>
2025-02-22 22:33:31 +0200

    Add prev migration

---
+++ RecipeApp.Infrastructure/Migrations/20250222202816_EditPreferredThemeAttribute.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RecipeApp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EditPreferredThemeAttribute : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<byte>(
+                name: "PreferredTheme",
+                table: "AspNetUsers",
+                type: "tinyint",
+                nullable: false,
+                defaultValue: (byte)1,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PreferredTheme",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(byte),
+                oldType: "tinyint",
+                oldDefaultValue: (byte)1);
+        }
+    }
+}
